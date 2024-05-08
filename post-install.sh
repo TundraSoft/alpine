4c1851bb1b85157f5345f7c9f1ba768f551245d9
Abhinav A V <36784+abhai2k@users.noreply.github.com>
2024-05-09 02:47:07 +0530

    Base attempt

---
+++ post-install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+# Copyright (c) 2018-2020 Dave Hall <skwashd@gmail.com>
+# MIT Licensed, see LICENSE for more information.
+#
+
+# Catch errors
+set -ex
+
+# Ensure certs are up to date
+update-ca-certificates
+
+# make saure we have the latest packages
+/sbin/apk update
+/sbin/apk upgrade
+
+apk add --no-cache tzdata wget libintl gettext shadow curl jq
+cp /usr/bin/envsubst /usr/local/bin/envsubst
+wget https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-noarch.tar.xz
+wget https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-symlinks-noarch.tar.xz
+wget https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/syslogd-overlay-noarch.tar.xz -P /tmp/
+if [ "${TARGETARCH}" == "amd64" ];
+then
+  wget https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-x86_64.tar.xz -O /tmp/s6-overlay-arch.tar.xz;
+fi
+if [ "${TARGETARCH}" == "arm64" ] || [[ "${TARGETARCH}" == "arm" && "${TARGETVARIANT}" == 'v8' ]]
+then
+  wget https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-aarch64.tar.xz -O /tmp/s6-overlay-arch.tar.xz
+fi
+if [ "${TARGETARCH}" == "arm" ] && [[ "${TARGETVARIANT}" == 'v7' || "${TARGETVARIANT}" == 'v6' ]]
+then
+  wget https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-armhf.tar.xz -O /tmp/s6-overlay-arch.tar.xz
+fi
+tar -C / -Jxpf /tmp/s6-overlay-noarch.tar.xz
+tar -C / -Jxpf /tmp/s6-overlay-arch.tar.xz
+tar -C / -Jxpf /tmp/s6-overlay-symlinks-noarch.tar.xz
+tar -C / -Jxpf /tmp/syslogd-overlay-noarch.tar.xz
+rm -rf /tmp/* /var/cache/apk/*
+apk del wget gettext -r
+# Add a standard user.
+addgroup -g ${PGID} tundra
+adduser -DH -s /sbin/nologin -u ${PUID} tundra -G tundra
